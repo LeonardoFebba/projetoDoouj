de7ea3c3294f9b642dd390c62648e3a952042db6
Leo Febba <leonardofebba@hotmail.com>
2023-10-15 01:07:08 -0300

    Added the tab Icon, Google form and minor changes on HTML

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ const texts = [
     "Inovação e excelência - nosso compromisso com o futuro."
 ];
 
-let currentTextIndex = 0; // Índice atual do texto
-let currentIndex = 0; // Índice atual do caractere no texto atual
-const speed = 50; // Velocidade de digitação em milissegundos
-const repeatDelay = 2000; // Tempo de pausa entre os textos em milissegundos
+let currentTextIndex = 0; 
+let currentIndex = 0;
+const speed = 50; 
+const repeatDelay = 2000; 
 
 function typeWriter() {
     if (currentTextIndex < texts.length) {
@@ -17,41 +17,35 @@ function typeWriter() {
             currentIndex++;
             setTimeout(typeWriter, speed);
         } else {
-            // Passar para o próximo texto após uma pausa
             setTimeout(() => {
                 currentIndex = 0;
                 currentTextIndex++;
-                document.getElementById("content").innerHTML = ""; // Limpar o texto anterior
-                typeWriter(); // Chamar a função novamente para o próximo texto
+                document.getElementById("content").innerHTML = "";
+                typeWriter();
             }, repeatDelay);
         }
     } else {
-        // Se todos os textos foram exibidos, reinicie o ciclo
         currentTextIndex = 0;
-        typeWriter(); // Chamar a função novamente para iniciar o ciclo novamente
+        typeWriter(); 
     }
 }
-
-// Iniciar a máquina de escrever quando a página carregar
 window.onload = function () {
     typeWriter();
 };
 
 const menuLinks = document.querySelectorAll('.menu-nav a');
 
-// Adicione um evento de clique a cada link do menu
+
 menuLinks.forEach(link => {
   link.addEventListener('click', (event) => {
-    event.preventDefault(); // Impede o comportamento padrão de clicar no link
+    event.preventDefault();
 
-    // Obtenha o ID da seção a ser navegada a partir do atributo href do link
     const targetId = link.getAttribute('href').substring(1);
 
-    // Selecione a seção alvo com base no ID
+
     const targetSection = document.getElementById(targetId);
 
     if (targetSection) {
-      // Use o método scrollIntoView para rolar suavemente para a seção alvo
       targetSection.scrollIntoView({ behavior: 'smooth' });
     }
   });
